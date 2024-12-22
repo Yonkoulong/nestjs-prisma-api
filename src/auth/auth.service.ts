3fac55395e354ddd066165c47824667ade40e4d6
longyonkou <nguyenhailong270921@gmail.com>
2024-12-22 17:33:44 +0700

    [Learn][Nest, Prisma]

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,86 @@
+import {
+  ForbiddenException,
+  Injectable,
+  PayloadTooLargeException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+import * as argon from 'argon2';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable({})
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signIn(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signUp(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+        throw error;
+      }
+    }
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+
+@Controller('user')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser() {}
+}
